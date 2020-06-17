4ab4f87adbe9785d71f545bf9a641c3100a3406e
EluneyMorgadoUgarte <65728353+EluneyDMU@users.noreply.github.com>
2020-06-17 14:01:00 +0200

    Add files via upload

---
+++ 02_array.js
@@ -0,0 +1,22 @@
+let someArrayNumbers = [1, 2, 3, 4];
+let someArrayString = ["Smart", "Ninja"];
+let someArrayMixed = ["Smart", 1, 38, 22.0, true];
+let someObject = {
+    //Key : Value
+    name: "Matt",
+    age: 22,
+    admin: false
+};
+let someArrayBooleans = [true,false]
+let someArrayWords = ["Oh it's a orange juice"]
+
+console.log(someArrayBooleans);
+console.log(someArrayWords);
+
+/*
+AUFGABE
+1) 2 Arrays anlegen
+- 1 Array mit Boolean Werten
+- 1 Array mit String Werten
+2) Ausgabe in der Konsole des Browsers
+*/
---
+++ 03_ifelse.js
@@ -0,0 +1,21 @@
+let mood = "sad";
+
+if (mood === "happy") {
+    console.log("HAPPY");
+} else {
+    console.log("Oh nooooooo");
+}
+
+let nice = "scary"
+
+if (nice === "scary") {
+    console.log("Tentacool");
+}else {
+    console.log(Ink);
+}
+
+/* 
+AUFGABE
+1) Defintion einer Variable als Bedingung
+2) Programmiere eine If-Else-Struktur für diese Bedingung
+*/
