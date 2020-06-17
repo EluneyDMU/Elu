8d59d7659c56f6c4103e6657292d366a79c89091
EluneyMorgadoUgarte <65728353+EluneyDMU@users.noreply.github.com>
2020-06-17 14:02:22 +0200

    Add files via upload

---
+++ 10_javascript_introduction/05_functions.js
@@ -0,0 +1,20 @@
+function sum(x, y) {
+    let ergebnis = x + y;
+    return ergebnis;
+}
+
+sum(10, 2);
+console.log("Ergebnis = " + sum(2, 3));
+
+function sum(x,y){
+    let ergebnis = x / y;
+    return ergebnis;
+}
+
+sum (64,8);
+console.log("Ergebnis = " + sum(64,8));
+/*
+AUFGABE:
+1) Funktion anlegen für Subtraktion oder Multiplikation oder Division
+2) Aufruf dieser Funktion
+*/
